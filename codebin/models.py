f39456de928328f598d92bcaeffd6139dbd7ec56
qwad1000 <qwad1000@ukr.net>
2016-06-14 22:48:39 +0300

    Make snippet model

--- codebin/models.py
+++ codebin/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+
+class Snippet(models.Model):
+    title = models.CharField(max_length=128)
+    save_name = models.CharField(max_length=128)
+    created_date = models.DateTimeField(default=timezone.now)
+
