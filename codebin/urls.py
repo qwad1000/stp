86692eebe44d6667df661352233b905b56a3cb08
qwad1000 <qwad1000@ukr.net>
2016-06-16 00:46:08 +0300

    Add snippet view

--- codebin/urls.py
+++ codebin/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.snippet_list, name='snippet_list')
+    url(r'^$', views.snippet_list, name='snippet_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.snippet_view, name='snippet_view')
 ]
--- codebin/views.py
+++ codebin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Snippet
 # Create your views here.
@@ -7,3 +7,9 @@ from .models import Snippet
 def snippet_list(request):
     snippets = Snippet.objects.filter(created_date__lte=timezone.now()).order_by('created_date')[:10]
     return render(request, 'codebin/snippet_list.html', {'snippets': snippets})
+
+
+def snippet_view(request, pk):
+    snippet = get_object_or_404(Snippet, pk=pk)
+    content = snippet.get_content()
+    return render(request, 'codebin/snippet_view.html', {'snippet': snippet, 'content': content})
